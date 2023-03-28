5a6e785bf293e090ebd37caabf4e2cf5a6418d30
johndesk2205 <joaomarcos2205@gmail.com>
2023-03-28 15:58:10 -0300

    Resolução dos exercicios

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,16 +1,27 @@
 //códigos a serem reescritos
 
 // a)
-if(nome === "José"){
-	console.log("Oi, Zé!")
-} else {
+// if(nome === "José"){
+// 	console.log("Oi, Zé!")
+// } else {
+// 	console.log("Olá, " + nome)
+// }
+
+const nome = 'José'
+nome === 'José' ?
+	console.log("Oi, Zé!") :
 	console.log("Olá, " + nome)
-}
+
 
 
 // b)
-if(idade >= 18){
-	console.log("Pode tirar carteira de motorista!")
-} else {
+// if(idade >= 18){
+// 	console.log("Pode tirar carteira de motorista!")
+// } else {
+// 	console.log("Ainda não pode tirar carteira de motorista!")
+// }
+
+const idade = 17
+idade >= 18 ?
+	console.log("Pode tirar carteira de motorista!") :
 	console.log("Ainda não pode tirar carteira de motorista!")
-}
